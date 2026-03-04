25154e6a9c07f7a30a13cf9cc8c3ae632091555d
Dcardenas206 <danielcardenas206@hotmail.com>
2026-03-04 13:38:58 -0500

    user_data.sh fixed

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -5,14 +5,15 @@ exec > /home/ubuntu/userdata.log 2>&1
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt update -y
-sudo apt install -y docker.io docker-compose-plugin unzip curl awscli
+apt update -y
+apt install -y docker.io docker-compose-plugin unzip curl awscli
 
 systemctl enable docker
 systemctl start docker
 
-sudo mkdir -p /home/ubuntu/Web_Platform/docker
-sudo chown -R ubuntu:ubuntu /home/ubuntu/Web_Platform
+usermod -aG docker ubuntu
+mkdir -p /home/ubuntu/Web_Platform/docker
+chown -R ubuntu:ubuntu /home/ubuntu/Web_Platform
 
 cat << 'EOF' > /home/ubuntu/Web_Platform/deploy.sh
 #!/bin/bash
@@ -37,5 +38,3 @@ EOF
 
 chmod +x /home/ubuntu/Web_Platform/deploy.sh
 chown ubuntu:ubuntu /home/ubuntu/Web_Platform/deploy.sh
-
-sudo touch /home/ubuntu/USER_DATA_FINISHED
