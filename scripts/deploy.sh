36f14167ca5f0c582e66ef0c0346afddf8bddaea
Dcardenas206 <danielcardenas206@hotmail.com>
2026-03-11 13:17:51 -0500

    deploy.sh ECR_TOKEN v4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,14 +8,14 @@ cd /home/ubuntu/Web_Platform
 # Si la variable ECR_TOKEN existe, la usamos para loguearnos sin AWS CLI
 if [ -n "$ECR_TOKEN" ]; then
     echo "Autenticando en ECR con token del Pipeline..."
-    echo "$ECR_TOKEN" | docker login --username AWS --password-stdin "$ECR_REGISTRY"
+    echo "$ECR_TOKEN" | sudo docker login --username AWS --password-stdin "$ECR_REGISTRY"
 else
     echo "Error: No se proporcionó ECR_TOKEN"
     exit 1
 fi
 
 # 2. Descargar las imágenes nuevas (Pull)
-docker compose -f docker/docker-compose.prod.yml pull
+sudo docker compose -f docker/docker-compose.prod.yml pull
 
 # 3. Levantar los servicios (recreando solo lo necesario)
-docker compose -f docker/docker-compose.prod.yml up -d --remove-orphans
+sudo docker compose -f docker/docker-compose.prod.yml up -d --remove-orphans
